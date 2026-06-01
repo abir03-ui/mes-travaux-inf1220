a35bb6ff42306bc927b82f3c2f6e6dd4d095bcdb
abir03-ui <abir_antri@yahoo.com>
2026-06-01 15:40:18 -0400

    Add additional output statements in Bonjour.java
    Modification du fichier Bonjour.java - ajout de commentaires Module 3

--- Bonjour.java
+++ Bonjour.java
@@ -3,5 +3,8 @@ public class Bonjour {
         System.out.println("Bonjour le monde !");
         System.out.println("Je suis Abir et je travaille sur INF 1220");
         System.out.println("Ce fichier est sur GitHub !");
+                System.out.println("-----------------------------");
+        System.out.println("Module 3 - Structures de contrôle");
+        System.out.println("J'apprends les boucles et les if !");
     }
 }
