1b9ec18d6db13eb30a4e881ed63e934b85be9d1a
abir03-ui <abir_antri@yahoo.com>
2026-06-01 15:27:03 -0400

    Add Bonjour class with main method

---
+++ Bonjour.java
@@ -0,0 +1,7 @@
+public class Bonjour {
+    public static void main(String[] args) {
+        System.out.println("Bonjour le monde !");
+        System.out.println("Je suis Abir et je travaille sur INF 1220");
+        System.out.println("Ce fichier est sur GitHub !");
+    }
+}
