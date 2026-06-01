89848ce0ff7bb63093132e211ea4c758772b361b
abir03-ui <abir_antri@yahoo.com>
2026-06-01 15:44:55 -0400

    Fix indentation and update output message

--- Bonjour.java
+++ Bonjour.java
@@ -1,9 +1,10 @@
 public class Bonjour {
+    
     public static void main(String[] args) {
         System.out.println("Bonjour le monde !");
         System.out.println("Je suis Abir et je travaille sur INF 1220");
         System.out.println("Ce fichier est sur GitHub !");
-                System.out.println("-----------------------------");
+        System.out.println("-----------------------------");
         System.out.println("Module 3 - Structures de contrôle");
         System.out.println("J'apprends les boucles et les if !");
     }
